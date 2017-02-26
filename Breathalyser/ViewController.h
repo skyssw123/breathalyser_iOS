8e24f56f9c483a0909e05a0e62a7f288867b7301
Thomas Sin <skyssw123@gmail.com>
2017-02-25 23:41:14 -0500

    Added Dial graph to UI

--- Breathalyser/ViewController.h
+++ Breathalyser/ViewController.h
@@ -7,9 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <NuweScoreCharts/NuweScoreCharts.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <NUDialChartDataSource, NUDialChartDelegate, NUBarChartDelegate, NUBarChartDataSource>
 
+@property (strong, nonatomic) IBOutlet NUDialChart *BigDialChart;
+@property (strong, nonatomic) IBOutlet NUDialChart *TopDialChart1;
+@property (strong, nonatomic) IBOutlet NUDialChart *TopDialChart2;
+
+@property (strong, nonatomic) IBOutlet NUDialChart *BottomDialChart1;
+@property (strong, nonatomic) IBOutlet NUDialChart *BottomDialChart2;
+@property (strong, nonatomic) IBOutlet NUDialChart *BottomDialChart3;
+
+@property (strong, nonatomic) IBOutlet NUBarChart *LandBarChart;
 
 @end
-
